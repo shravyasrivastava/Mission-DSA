ccaab28f7d919fe5e642171f9726f8b077e47bd2
shravyasrivastava <2k23csaiml2311299@gmail.com>
2025-09-18 21:21:08 +0530

    Time: 0 ms (100%), Space: 18.2 MB (21.75%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,9 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans=[]
+        def dfs(s:int,path:list[int])->None:
+            ans.append(path)
+            for i in range(s,len(nums)):
+                dfs(i+1,path+[nums[i]])
+        dfs(0,[])
+        return ans
