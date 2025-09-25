1375349ced3b7811abdd775549cde206b491fc33
shravyasrivastava <2k23csaiml2311299@gmail.com>
2025-09-25 16:43:04 +0530

    Time: 0 ms (100%), Space: 17.8 MB (59.27%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sumNumbers(self, root: TreeNode | None) -> int:
+        ans=0
+        def dfs(root:TreeNode | None,path:int)->int:
+            nonlocal ans
+            if not root:
+                return
+            if not root.left and not root.right:
+                ans+=path*10+root.val
+                return
+            dfs(root.left,path*10+root.val)
+            dfs(root.right,path*10+root.val)
+        dfs(root,0)
+        return ans
+        
