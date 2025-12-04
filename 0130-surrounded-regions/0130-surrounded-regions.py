4af3ffc96dc3e4eddc392351ae0a70cea2ada916
shravyasrivastava <2k23csaiml2311299@gmail.com>
2025-12-04 09:55:24 +0530

    Time: 3 ms (99.18%), Space: 16.9 MB (60.7%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,28 +1,30 @@
-class Solution:
-  def solve(self, board: list[list[str]]) -> None:
-    if not board:
-      return
-
-    m = len(board)
-    n = len(board[0])
-
-    def dfs(i: int, j: int) -> None:
-      """Marks the grids with 'O' that stretch from the four sides to '*'."""
-      if i < 0 or i == m or j < 0 or j == n:
-        return
-      if board[i][j] != 'O':
-        return
-      board[i][j] = '*'
-      dfs(i + 1, j)
-      dfs(i - 1, j)
-      dfs(i, j + 1)
-      dfs(i, j - 1)
-
-    for i in range(m):
-      for j in range(n):
-        if i * j == 0 or i == m - 1 or j == n - 1:
-          dfs(i, j)
-
-    for row in board:
-      for i, c in enumerate(row):
-        row[i] = 'O' if c == '*' else 'X'
+class Solution(object):
+    def solve(self, board):
+        if not board or not board[0]:
+            return
+        rows,cols=len(board),len(board[0])
+        def dfs(r,c):
+            if r<0 or c<0 or r>=rows or c>=cols or board[r][c]!='O':
+                return
+            board[r][c]='S'
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+        for i in range(rows):
+            if board[i][0]=='O':
+                dfs(i,0)
+            if board[i][cols-1]=='O':
+                dfs(i,cols-1)
+        for j in range(cols):
+            if board[0][j]=='O':
+                dfs(0,j)
+            if board[rows-1][j]=='O':
+                dfs(rows-1,j)
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c]=='O':
+                    board[r][c]='X'
+                elif board[r][c]=='S':
+                    board[r][c]='O'
+        
